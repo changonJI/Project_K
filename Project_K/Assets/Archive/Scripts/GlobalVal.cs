93916ed8ee6d3dc1d3f4ab6a5d994a5bac1da7bf
DESKTOP\지찬곤 <jcg_dev@naver.com>
2024-06-25 00:33:14 +0900

    - 구글시트_공용 string 추가

--- Project_K/Assets/Archive/Scripts/GlobalVal.cs
+++ Project_K/Assets/Archive/Scripts/GlobalVal.cs
@@ -7,6 +7,16 @@ using UnityEngine;
 /// </summary>
 public static class GlobalVal
 {
-    public const string str_myName = "JCG";
+    /// <summary>
+    /// 복사해온 URL 에서 edit?usp=sharing 제거후 제거된 부분에 export?format=tsv&range=시트 범위&gid=시트ID값
+    /// </summary>
+    /// <param name="address">https:// ~ /</param>
+    /// <param name="range">구글 시트 Range</param>
+    /// <param name="sheetID">시트 고유 ID</param>
+    /// <returns></returns>
+    public static string GetGoogleSheetAddress(string address, string range, string sheetID)
+    {
+        return $"{address}/export?format=tsv&range={range}&gid={sheetID}";
+    }
 
 }
